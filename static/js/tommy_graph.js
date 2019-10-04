abdfe8c9b37c9b44cb571d724e8cc8e04dec2d67
Mike <mcandersonmusic@gmail.com>
2019-10-03 19:19:13 -0500

    graph files

---
+++ static/js/tommy_graph.js
@@ -0,0 +1,26 @@
+//Create function for making the graph
+function tommy_graph(pokemens){
+
+//START CODING HERE
+
+
+
+
+//Use d3.json to fetch the data for the current pokemon
+  //Use d3 to select the searchbar with ID of "#searchbar"
+
+  //Use `.html("")` to clear any existing data
+
+  //Use `d3.json` to fetch the sample data for the plot
+
+
+  //Create bar chart for the data
+
+  //
+
+
+
+
+
+
+};
