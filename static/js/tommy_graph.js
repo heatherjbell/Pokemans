7f985894d9ddc9d78b4402018491f0f1e897fe69
Thomas Stokes <thomas.stokes2477@gmail.com>
2019-10-03 19:43:18 -0500

    Adding Tommy's first graph

--- static/js/tommy_graph.js
+++ static/js/tommy_graph.js
@@ -1,8 +1,29 @@
 //Create function for making the graph
 function tommy_graph(pokemens){
 
+  
 //START CODING HERE
+  var trace1 = {
+    x: "HP",
+    y: pokemens.HP,
+    type: "bar"
+  };
 
+  var trace2 = {
+    x: "Attack",
+    y: pokemens.Attack,
+    type: "bar"
+  };
+
+  var trace3 = {
+    x: "Defense",
+    y: pokemens.Defense,
+    type: "bar"
+  };
+  
+  var data = [trace1, trace2, trace3];
+
+  Plotly.newPlot("graphone", data, "");
 
 
 
@@ -24,3 +45,5 @@ function tommy_graph(pokemens){
 
 
 };
+
+
