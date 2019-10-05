737461224c34ee2fd0c1e7c8362ddb0ca2331e95
heatherjbell <heatherspixels@gmail.com>
2019-10-05 18:32:30 -0500

    Update mongo_pics_and_stats.js

--- static/js/mongo_pics_and_stats.js
+++ static/js/mongo_pics_and_stats.js
@@ -4,7 +4,7 @@ function scrape_for_stats() {
   var pokemon = d3.select("#searchbar").value;
   d3.json("/stats").then((pokemonStats) => {
     pokemonStats.forEach((monster) => {
-      if (monster.Name == pokemon){
+      if (monster.Name == pokemon) {
         console.log(monster.Name);
         console.log(monster.HP);
         console.log(monster.Attack);
@@ -18,29 +18,27 @@ var stats_panel_heading = d3.select("#stringpanelheading");
 var stats_panel = d3.select("#statspanel");
 var picture = d3.select("#photobox");
 
-d3.select("#searchbutton").on("click", function() {
+d3.select("#searchbutton").on("click", function () {
   var pokemon = d3.select("#searchbar").property("value");
   d3.select("#statspanel").html("");
   d3.select("#stringpanelheading").html("");
   d3.select("#photobox").html("");
   d3.json("/stats").then(result => {
     result.forEach(pokeguy => {
-      if (pokeguy.Name == pokemon){
+      if (pokeguy.Name == pokemon) {
         tommy_graph(pokeguy);
         heather_graph(pokeguy);
         stats_panel_heading.append("h2").text(`${pokeguy.Name}`)
         stats_panel.append("h5").text(`Number: #${pokeguy.Number}`);
-        if (pokeguy.Type_2 == null){
+        if (pokeguy.Type_2 == null) {
           stats_panel.append("h5").text(`Type: ${pokeguy.Type_1}`);
-        }
-        else {
+        } else {
           stats_panel.append("h5").text(`Type: ${pokeguy.Type_1} \/ ${pokeguy.Type_2}`);
         }
         stats_panel.append("h5").text(`Generation: ${pokeguy.Generation}`);
-        if (pokeguy.Legendary == true){
+        if (pokeguy.Legendary == true) {
           stats_panel.append("h5").text("Legendary: YUP!!!").style("color", "#e0a13a");
-        }
-        else{
+        } else {
           stats_panel.append("h5").text("Legendary: No, he's lame");
         }
       };
@@ -48,14 +46,14 @@ d3.select("#searchbutton").on("click", function() {
   });
   d3.json("/images").then(result => {
     result.forEach(pokeguy => {
-      if (pokeguy.name == pokemon){
+      if (pokeguy.name == pokemon) {
         picture.append("svg")
-        .attr('width', 250)
-        .attr('height', 250)
-        .append('image')
-        .attr('xlink:href', pokeguy.url)
-        .attr('width', 250)
-        .attr('height', 250)
+          .attr('width', 250)
+          .attr('height', 250)
+          .append('image')
+          .attr('xlink:href', pokeguy.url)
+          .attr('width', 250)
+          .attr('height', 250)
       };
     })
   })
