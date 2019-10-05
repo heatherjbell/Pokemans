d4ddb6fe3b58b61875e796d7e6159beddffc7d70
heatherjbell <heatherspixels@gmail.com>
2019-10-05 09:55:02 -0500

    update heatherplot

--- static/js/heather_graph.js
+++ static/js/heather_graph.js
@@ -2,7 +2,11 @@
 function heather_graph(pokegirl){
 
 //START CODING HERE
-
+  var trace1 = {
+    x: ["Special Attack", "Special Defense", "Speed"],
+    y: [pokegirl.Special_Attack, pokegirl.Special_Defense, pokegirl.Speed],
+    type: "bar"
+  };
 
 
 
@@ -12,9 +16,10 @@ function heather_graph(pokegirl){
   //Use `.html("")` to clear any existing data
 
   //Use `d3.json` to fetch the sample data for the plot
-
+  var data = [trace1]
 
   //Create bar chart for the data
+  Plotly.newPlot("graphtwo", data, "");
 
   //
 
